c965b67ad410126a7e86ac026dfa5b678f1cf61e
Anna <vinnik.anna@rambler.ru>
2022-08-30 15:45:03 +0700

    Add slider in Portfolio section

--- script/app.js
+++ script/app.js
@@ -3,9 +3,26 @@
 const sidebar = document.querySelector(".sidebar");
 const sidebarItems = sidebar.querySelectorAll(".nav__item");
 const mainSlider = document.querySelector(".main-slider");
-const slides = mainSlider.querySelectorAll("div");
+const slides = mainSlider.querySelectorAll(".card");
 const slidesCount = slides.length;
 const container = document.querySelector(".container");
+
+const portfolioSlider = document.querySelector(".portfolio-slider");
+const portfolioSlides = portfolioSlider.querySelectorAll("div");
+
+portfolioSlides.forEach( (elem) => {
+    elem.addEventListener('click', (event) => {
+        removeActive(portfolioSlides);
+        event.target.classList.add("active");
+    })
+});
+
+function removeActive(slides) {
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+}
+
 
 let activeSlide = 0;
 
@@ -16,20 +33,7 @@ sidebarItems.forEach( (elem) => {
 function changeSlide(event) {
     activeSlide = Array.from(slides).findIndex(elem => elem.id === event.target.textContent);
 
-    // if (direction === "up") {
-    //     activeSlide++;
-    //     if (activeSlide === slidesCount) {
-    //         activeSlide = 0;
-    //     }
-    // } else if (direction === "down") {
-    //     activeSlide--;
-    //     if(activeSlide < 0) {
-    //         activeSlide = slidesCount - 1;
-    //     }
-    // }
-
-
     const height = container.clientHeight;
     mainSlider.style.transform = `translateY(-${activeSlide * height}px)`;
-    // sidebar.style.transform = `translateY(${activeSlide * height}px)`;
 }
+
