129480ec6a731863e536b4ccb0fa4553679c145a
Anna <vinnik.anna@rambler.ru>
2022-08-30 11:35:54 +0700

    Add sidebar

--- script/app.js
+++ script/app.js
@@ -1,27 +1,35 @@
 "use strict";
 
 const sidebar = document.querySelector(".sidebar");
+const sidebarItems = sidebar.querySelectorAll(".nav__item");
 const mainSlider = document.querySelector(".main-slider");
-const slidesCount = mainSlider.querySelectorAll("div").length;
+const slides = mainSlider.querySelectorAll("div");
+const slidesCount = slides.length;
 const container = document.querySelector(".container");
 
 let activeSlide = 0;
 
-function changeSlide(direction) {
-    if (direction === "up") {
-        activeSlide++;
-        if (activeSlide === slidesCount) {
-            activeSlide = 0;
-        }
-    } else if (direction === "down") {
-        activeSlide--;
-        if(activeSlide < 0) {
-            activeSlide = slidesCount - 1;
-        }
-    }
+sidebarItems.forEach( (elem) => {
+    elem.addEventListener('click', changeSlide);
+});
+
+function changeSlide(event) {
+    activeSlide = Array.from(slides).findIndex(elem => elem.id === event.target.textContent);
+
+    // if (direction === "up") {
+    //     activeSlide++;
+    //     if (activeSlide === slidesCount) {
+    //         activeSlide = 0;
+    //     }
+    // } else if (direction === "down") {
+    //     activeSlide--;
+    //     if(activeSlide < 0) {
+    //         activeSlide = slidesCount - 1;
+    //     }
+    // }
 
 
     const height = container.clientHeight;
     mainSlider.style.transform = `translateY(-${activeSlide * height}px)`;
-    sidebar.style.transform = `translateY(${activeSlide * height}px)`;
+    // sidebar.style.transform = `translateY(${activeSlide * height}px)`;
 }
